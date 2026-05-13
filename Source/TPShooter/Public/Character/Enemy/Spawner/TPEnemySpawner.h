f54d5ffea8af4d7ef7b83705709b7197ed7cf49a
Alejandra05gsr <79392781+Alejandra05gsr@users.noreply.github.com>
2026-05-13 07:42:03 -0600

    Correcciones generales
    Corrección de colisiones, pool, meshes y bugs

---
+++ Source/TPShooter/Public/Character/Enemy/Spawner/TPEnemySpawner.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TPEnemySpawner.generated.h"
+
+class UBoxComponent;
+class AATPCharacterBase;
+
+UCLASS()
+class TPSHOOTER_API ATPEnemySpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ATPEnemySpawner();
+
+protected:
+	virtual void BeginPlay() override;
+
+	// Función para el temporizador
+	void TickSpawn();
+
+	// Saca un enemigo de la "piscina"
+	void GetEnemyFromPool();
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* SpawnArea;
+
+	/** Configuración del Pool */
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+	TSubclassOf<AATPCharacterBase> EnemyClass;
+
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+	int32 PoolSize = 10;
+
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+	float SpawnInterval = 3.f;
+
+private:
+	// Nuestra lista de enemigos reutilizables
+	UPROPERTY()
+	TArray<AATPCharacterBase*> EnemyPool;
+
+	FTimerHandle SpawnTimerHandle;
+};
